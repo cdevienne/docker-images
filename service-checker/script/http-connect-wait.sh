4d63e62e82845e34a92f0dfee6110827976a047b
Marc Lennox <marc.lennox@gmail.com>
2014-08-21 15:22:29 -0400

    Added service checker dockerfile

---
+++ service-checker/script/http-connect-wait.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set the service URL
+SERVICE_URL=${SERVICE_URL:-$1}
+WAIT_SECONDS=5
+
+# Exit with error if SERVICE_URL wasn't provided
+if [ "${SERVICE_URL}" == "" ]; then
+  echo "The service URL must be provided either with the SERVICE_URL environment variable or the first argument"
+  exit 1
+fi
+
+while curl "${SERVICE_URL}" > /dev/null 2>&1; test ${PIPESTATUS[0]} -gt 0; do
+  echo "Service at ${SERVICE_URL} not listening, waiting ${WAIT_SECONDS} seconds to try again..."
+  sleep ${WAIT_SECONDS}
+done
